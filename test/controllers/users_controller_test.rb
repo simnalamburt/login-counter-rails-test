27250e88637fc7ff6921b290b938e24ec8628b42
Hyeon Kim <simnalamburt@gmail.com>
2015-04-20 19:44:16 +0900

    Write tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#signup_success' do
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    json = JSON.parse response.body
+    assert json['user_name'] == 'testuser'
+    assert json['login_count'] == 1
+  end
+
+  test '#signup_failure_user' do
+    post :signup, {'username' => 'test', 'password' => 'testpass'}
+    json = JSON.parse response.body
+    assert json['error_code'] == -1
+  end
+
+  test '#signup_failure_pwd' do
+    post :signup, {'username' => 'testuser', 'password' => 'test'}
+    json = JSON.parse response.body
+    assert json['error_code'] == -2
+  end
+
+  test '#signup_failure_pwd' do
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    json = JSON.parse response.body
+    assert json['user_name'] == 'testuser'
+    assert json['login_count'] == 1
+
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+    json = JSON.parse response.body
+    assert json['error_code'] == -3
+  end
 end
